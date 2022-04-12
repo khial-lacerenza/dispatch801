086a5ad6c522968f2477190d0a23685ead89bd78
Omar <omarkhial74@gmail.com>
2022-04-12 14:54:50 +0200

    adding OffreMateriel wich randomly generate an offer

--- src/main/java/com/lk/fret/OffreMateriel.java
+++ src/main/java/com/lk/fret/OffreMateriel.java
@@ -1,16 +1,39 @@
 package com.lk.fret;
 
-import java.util.Date;
+import java.util.Random;
 
 public class OffreMateriel {
+
+    // generate random numbers between n range
+    public int getRandomNumber(int min, int max) {
+        Random random = new Random();
+        return random.ints(min, max).findFirst().getAsInt();
+    }
+
     private int quantite, prix;
     private String produit;
-    private Date date;
+    private int nbMois;
 
-    public OffreMateriel(int quantite, int prix, String produit, Date date) {
-        this.quantite = quantite;
-        this.prix = prix;
-        this.produit = produit;
-        this.date = date;
+    public OffreMateriel() {
+        this.quantite = getRandomNumber(5, 20); // en miller d'unités
+        this.prix = getRandomNumber(10,370);   // en millier d'euros
+        this.produit = "Chaussettes";
+        this.nbMois = getRandomNumber(1,5);
+    }
+
+    public int getQuantite() {
+        return quantite;
+    }
+
+    public int getPrix() {
+        return prix;
+    }
+
+    public String getProduit() {
+        return produit;
+    }
+
+    public int getNbMois() {
+        return nbMois;
     }
 }
