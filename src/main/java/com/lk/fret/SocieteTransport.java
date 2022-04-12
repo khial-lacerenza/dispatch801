04090ed1e0f07d0a4322c9a05ffec8b9e17b2d9f
LuxemTheFez <37304100+LuxemTheFez@users.noreply.github.com>
2022-04-12 15:08:01 +0200

    [UPDATE] Ajout fonction ajouteOffre + id

--- src/main/java/com/lk/fret/SocieteTransport.java
+++ src/main/java/com/lk/fret/SocieteTransport.java
@@ -4,16 +4,19 @@ import org.jspace.Space;
 
 public class SocieteTransport implements Runnable {
 
+    private static int ID =0;
+    private int id;
     private Space tupleSpaceTransport;
     private OffreTransport offreTransport;
 
     public SocieteTransport(Space tupleSpaceTransport) {
+        this.id = ID++;
         this.tupleSpaceTransport = tupleSpaceTransport;
         this.offreTransport = new OffreTransport();
     }
 
-    public void offreTransport(String type, int quantite) throws InterruptedException {
-        tupleSpaceTransport.put(type, quantite);
+    public void ajouteOffre() throws InterruptedException {
+        tupleSpaceTransport.put("offre", offreTransport);
     }
 
     @Override
